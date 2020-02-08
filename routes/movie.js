d6a8bf0bae259a0de9ad62482596267214ee2f4b
Caner Gulcan <canergulcan@gmail.com>
2020-02-08 19:27:11 +0300

    read file endpoint table

--- routes/movie.js
+++ routes/movie.js
@@ -2,9 +2,30 @@ const express = require('express');
 const router = express.Router();
 
 //Models
-const Movie=require('../models/Movie');
+const Movie= require('../models/Movie');
 
-router.post('/', function(req, res, next) {
+router.get('/', (req,res)=>{
+   const promise=Movie.find({ });
+
+   promise.then((data)=> {
+    res.json(data);
+   }).catch((err)=>{
+       res.json(err);
+   });
+});
+
+router.get('/:movie_id', (req,res)=>{
+    const promise=Movie.findById(req.params.movie_id);
+
+    promise.then((movie)=>{
+        res.json(movie);
+    }).catch((err)=>{
+        res.json(err);
+    });
+});
+
+
+router.post('/', (req, res, next) => {
   const { title,imdb_score,category,country,year}=req.body;
 
 /* First Option :
