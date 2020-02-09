f16158f4434a4073b970d45cfdcca89456382b7e
Caner <canerglcn@gmail.com>
2020-02-09 12:34:56 +0300

    changed db connection option

--- helper/db.js
+++ helper/db.js
@@ -1,7 +1,9 @@
 const mongoose=require('mongoose');
 
 module.exports=()=>{
-    mongoose.connect('mongodb+srv://canerglcn:trail@freecluster-nrlr2.mongodb.net/test?retryWrites=true&w=majority');
+    mongoose.connect('mongodb+srv://canerglcn:trail@freecluster-nrlr2.mongodb.net/test?retryWrites=true&w=majority',{
+       useNewUrlParser:true,useFindAndModify:false,useCreateIndex:true,useUnifiedTopology:true
+    });
     mongoose.connection.on('open',()=>{
         console.log("MongoDb: Connected");
     });
