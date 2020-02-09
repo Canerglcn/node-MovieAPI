f6cac5e3805b1ff2e4ae16cc04bccaed0a2775fe
Caner Gulcan <canergulcan@gmail.com>
2020-02-09 16:51:52 +0300

    Movies,Directors scheme validations end point

--- models/Movie.js
+++ models/Movie.js
@@ -5,11 +5,28 @@ const MovieSchema=new Schema({
    director_id:Schema.Types.ObjectId,
    title:{
        type:String,
-       required:true
+       required:[true,' `{PATH}` is required. '],
+       maxlength:[25,' `{PATH}` field (`{VALUE}`), Must be less than ({MAXLENGTH}) characters.  '],
+       minlength:[2,'  `{PATH}` field (`{VALUE}`), Must be greater than ({MINLENGTH}) characters ']
+
    },
-    category:String,
-    country:String,
-    year:Number,
+    category:{
+       type:String,
+       maxlength:20,
+       minlength:3,
+
+    },
+    country:{
+      type:String,
+      maxlength:30,
+      minlength:4,
+
+    },
+    year:{
+       type:Number,
+        max:2040,
+        min:1900
+    },
     imdb_score:Number,
     createdAt:{
        type:Date,
