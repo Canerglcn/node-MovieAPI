6292890eab74386c6e1cb04e10a6f3ddc011b8c6
Caner Gulcan <canergulcan@gmail.com>
2020-02-09 15:03:52 +0300

    api/directors/ [delete] endpoint

--- routes/director.js
+++ routes/director.js
@@ -1,3 +1,4 @@
+const mongoose=require('mongoose');
 const express = require('express');
 const router = express.Router();
 
@@ -61,6 +62,91 @@ router.get('/', (req, res)=> {
     res.json(err);
   })
 });
+
+
+router.get('/:director_id', (req, res)=> {
+  const promise=Director.aggregate([
+    {
+      $match:{
+        '_id':mongoose.Types.ObjectId(req.params.director_id)
+      }
+    },
+    {
+      $lookup:{
+        from:'movies',
+        localField:'_id',
+        foreignField:'director_id',
+        as:'movies'
+      }
+    },
+    {
+      $unwind:{
+        path:'$movies',
+        preserveNullAndEmptyArrays:true
+      }
+    },
+    {
+      $group:{
+        _id: {
+          _id:'$_id',
+          name:'$name',
+          surname:'$surname',
+          bio:'$bio',
+        },
+        movies:{
+          $push:'$movies'
+        }
+      }
+    },
+    {
+      $project: {
+        _id:'$_id._id',
+        name:'$_id.name',
+        surname:'$_id.surname',
+        bio:'$_id.bio',
+        movies:'$movies'
+      }
+    }
+  ]);
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  })
+});
+
+router.put('/:director_id', (req,res,next)=>{
+  const promise=Director.findByIdAndUpdate(
+      req.params.director_id,
+      req.body,
+      {
+        new:true
+      }
+  );
+
+  promise.then((director)=>{
+    if(!director)
+      next({ message:'The director is not found',code:1 });
+    res.json(director);
+  }).catch((err)=>{
+    res.json(err);
+  });
+
+});
+
+router.delete('/:director_id', (req,res,next)=>{
+  const promise=Director.findByIdAndRemove(req.params.director_id);
+
+  promise.then((director)=>{
+    if(!director)
+      next({ message:'The director is not found',code:1 });
+    res.json({status:1});
+  }).catch((err)=>{
+    res.json(err);
+  });
+
+});
+
 
 
 module.exports = router;
